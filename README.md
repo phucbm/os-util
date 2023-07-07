43d733fc27244ae8978a0eaf28742b749af7d4eb
phucbm <hiphucbui@gmail.com>
2023-07-07 17:27:28 +0700

    refactor: rename repo

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# OS Util
+# OS Utility
--- dist/os-utility.module.js
+++ dist/os-utility.module.js
@@ -1,6 +1,6 @@
 /**!
- * OS Util v0.0.1
+ * OS Utility v0.0.1
  * @author phucbm
- * @homepage https://github.com/phucbm/os-util
+ * @homepage https://github.com/phucbm/os-utility
  * @license MIT 2023
  */var e={d:(o,t)=>{for(var r in t)e.o(t,r)&&!e.o(o,r)&&Object.defineProperty(o,r,{enumerable:!0,get:t[r]})},o:(e,o)=>Object.prototype.hasOwnProperty.call(e,o)},o={};function t(){console.log(1)}e.d(o,{y:()=>t});var r=o.y;export{r as getOptionsFromAttribute};
